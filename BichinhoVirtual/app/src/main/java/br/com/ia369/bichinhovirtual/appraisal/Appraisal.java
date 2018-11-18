4f63a6ca9b0de0d8634192601a6cde782b39fb34
Suelen <suelen.apsilva@gmail.com>
2018-11-18 20:20:48 -0200

    Adiciona metodos de appraisal para todas as emocoes

--- BichinhoVirtual/app/src/main/java/br/com/ia369/bichinhovirtual/appraisal/Appraisal.java
+++ BichinhoVirtual/app/src/main/java/br/com/ia369/bichinhovirtual/appraisal/Appraisal.java
@@ -10,14 +10,9 @@ import com.occ.rules.infra.RulesBuilder;
 
 import br.com.ia369.bichinhovirtual.model.EmotionVariables;
 
-public class Appraisal {
-    protected static final Double THRESHOLD = 0.3;
-    protected static final Double GLOBAL_VALUES = 0.5;
-    protected static final Double UNEXPECTEDNESS_FEAR = 1.0;
-    protected static final Double AROUSAL_FEAR = 2.0;
+class Appraisal {
 
-    private static final Double POSITIVE_VALUE = 0.3;
-    private static final Double NEGATIVE_VALUE = -1.0;//-0.3;
+    private static final Double THRESHOLD = 0.3;
 
     static Emotion evaluateFear(EmotionVariables emotionVariables) {
 
@@ -72,6 +67,229 @@ public class Appraisal {
         try {
             boolean isJoy = Evaluator.evaluate(emotion, model);
             if(isJoy) {
+                return emotion;
+            }
+        } catch (Exception ignored) {
+        }
+
+        return null;
+    }
+
+    static Emotion evaluateSadness(EmotionVariables emotionVariables) {
+
+        Model model = new Model();
+        model
+                .add(new Variable(VariableType.SENSE_OF_REALITY, emotionVariables.getSenseOfReality().toString()))
+                .add(new Variable(VariableType.PROXIMITY, emotionVariables.getProximity().toString()))
+                .add(new Variable(VariableType.UNEXPECTEDNESS, emotionVariables.getUnexpectedness().toString()))
+                .add(new Variable(VariableType.AROUSAL, emotionVariables.getArousal().toString()));
+
+        Rule rule = RulesBuilder.buildDisappointmentRule();
+        Emotion emotion = new Emotion("Sadness", rule, THRESHOLD);
+
+        if(emotionVariables.getDesirability() != null) {
+            Variable desirability = new Variable(VariableType.DESIRABILITY, emotionVariables.getDesirability().toString());
+            model.add(desirability);
+        }
+
+        if(emotionVariables.getLikelihood() != null) {
+            Variable likelihood = new Variable(VariableType.LIKELIHOOD, emotionVariables.getLikelihood().toString());
+            model.add(likelihood);
+        }
+
+        if(emotionVariables.getRealization() != null) {
+            Variable realization = new Variable(VariableType.REALIZATION, emotionVariables.getRealization().toString());
+            model.add(realization);
+        }
+
+        if(emotionVariables.getEffort() != null) {
+            Variable effort = new Variable(VariableType.EFFORT, emotionVariables.getEffort().toString());
+            model.add(effort);
+        }
+
+        try {
+            boolean isSadness = Evaluator.evaluate(emotion, model);
+            if(isSadness) {
+                return emotion;
+            }
+        } catch (Exception ignored) {
+        }
+
+        return null;
+    }
+
+    static Emotion evaluateReproach(EmotionVariables emotionVariables) {
+
+        Model model = new Model();
+        model
+                .add(new Variable(VariableType.SENSE_OF_REALITY, emotionVariables.getSenseOfReality().toString()))
+                .add(new Variable(VariableType.PROXIMITY, emotionVariables.getProximity().toString()))
+                .add(new Variable(VariableType.UNEXPECTEDNESS, emotionVariables.getUnexpectedness().toString()))
+                .add(new Variable(VariableType.AROUSAL, emotionVariables.getArousal().toString()));
+
+        Rule rule = RulesBuilder.buildReproachRule();
+        Emotion emotion = new Emotion("Disgust", rule, THRESHOLD);
+
+        if(emotionVariables.getPraiseworthiness() != null) {
+            Variable praiseworthiness = new Variable(VariableType.PRAISEWORTHINESS, emotionVariables.getPraiseworthiness().toString());
+            model.add(praiseworthiness);
+        }
+
+        if(emotionVariables.getExpectationDeviation() != null) {
+            Variable expectationDeviation = new Variable(VariableType.EXPECTATION_DEVIATION, emotionVariables.getExpectationDeviation().toString());
+            model.add(expectationDeviation);
+        }
+
+        try {
+            boolean isDisgust = Evaluator.evaluate(emotion, model);
+            if(isDisgust) {
+                return emotion;
+            }
+        } catch (Exception ignored) {
+        }
+
+        return null;
+    }
+
+    static Emotion evaluateAnger(EmotionVariables emotionVariables) {
+
+        Model model = new Model();
+        model
+                .add(new Variable(VariableType.SENSE_OF_REALITY, emotionVariables.getSenseOfReality().toString()))
+                .add(new Variable(VariableType.PROXIMITY, emotionVariables.getProximity().toString()))
+                .add(new Variable(VariableType.UNEXPECTEDNESS, emotionVariables.getUnexpectedness().toString()))
+                .add(new Variable(VariableType.AROUSAL, emotionVariables.getArousal().toString()));
+
+        Rule rule = RulesBuilder.buildAngerRule();
+        Emotion emotion = new Emotion("Anger", rule, THRESHOLD);
+
+        if(emotionVariables.getDesirability() != null) {
+            Variable desirability = new Variable(VariableType.DESIRABILITY, emotionVariables.getDesirability().toString());
+            model.add(desirability);
+        }
+
+        if(emotionVariables.getPraiseworthiness() != null) {
+            Variable praiseworthiness = new Variable(VariableType.PRAISEWORTHINESS, emotionVariables.getPraiseworthiness().toString());
+            model.add(praiseworthiness);
+        }
+
+        if(emotionVariables.getExpectationDeviation() != null) {
+            Variable expectationDeviation = new Variable(VariableType.EXPECTATION_DEVIATION, emotionVariables.getExpectationDeviation().toString());
+            model.add(expectationDeviation);
+        }
+
+        try {
+            boolean isAnger = Evaluator.evaluate(emotion, model);
+            if(isAnger) {
+                return emotion;
+            }
+        } catch (Exception ignored) {
+        }
+
+        return null;
+    }
+
+    static Emotion evaluateSatisfaction(EmotionVariables emotionVariables) {
+
+        Model model = new Model();
+        model
+                .add(new Variable(VariableType.SENSE_OF_REALITY, emotionVariables.getSenseOfReality().toString()))
+                .add(new Variable(VariableType.PROXIMITY, emotionVariables.getProximity().toString()))
+                .add(new Variable(VariableType.UNEXPECTEDNESS, emotionVariables.getUnexpectedness().toString()))
+                .add(new Variable(VariableType.AROUSAL, emotionVariables.getArousal().toString()));
+
+        Rule rule = RulesBuilder.buildSatisfactionRule();
+        Emotion emotion = new Emotion("Satisfaction", rule, THRESHOLD);
+
+        if(emotionVariables.getDesirability() != null) {
+            Variable desirability = new Variable(VariableType.DESIRABILITY, emotionVariables.getDesirability().toString());
+            model.add(desirability);
+        }
+
+        if(emotionVariables.getLikelihood() != null) {
+            Variable likelihood = new Variable(VariableType.LIKELIHOOD, emotionVariables.getLikelihood().toString());
+            model.add(likelihood);
+        }
+
+        if(emotionVariables.getRealization() != null) {
+            Variable realization = new Variable(VariableType.REALIZATION, emotionVariables.getRealization().toString());
+            model.add(realization);
+        }
+
+        if(emotionVariables.getEffort() != null) {
+            Variable effort = new Variable(VariableType.EFFORT, emotionVariables.getEffort().toString());
+            model.add(effort);
+        }
+
+        try {
+            boolean isSatisfaction = Evaluator.evaluate(emotion, model);
+            if(isSatisfaction) {
+                return emotion;
+            }
+        } catch (Exception ignored) {
+        }
+
+        return null;
+    }
+
+    static Emotion evaluateDistress(EmotionVariables emotionVariables) {
+
+        Model model = new Model();
+        model
+                .add(new Variable(VariableType.SENSE_OF_REALITY, emotionVariables.getSenseOfReality().toString()))
+                .add(new Variable(VariableType.PROXIMITY, emotionVariables.getProximity().toString()))
+                .add(new Variable(VariableType.UNEXPECTEDNESS, emotionVariables.getUnexpectedness().toString()))
+                .add(new Variable(VariableType.AROUSAL, emotionVariables.getArousal().toString()));
+
+        Rule rule = RulesBuilder.buildDistressRule();
+        Emotion emotion = new Emotion("Distress", rule, THRESHOLD);
+
+        if(emotionVariables.getDesirability() != null) {
+            Variable desirability = new Variable(VariableType.DESIRABILITY, emotionVariables.getDesirability().toString());
+            model.add(desirability);
+        }
+
+        try {
+            boolean isDistress = Evaluator.evaluate(emotion, model);
+            if(isDistress) {
+                return emotion;
+            }
+        } catch (Exception ignored) {
+        }
+
+        return null;
+    }
+
+    static Emotion evaluateGratitude(EmotionVariables emotionVariables) {
+
+        Model model = new Model();
+        model
+                .add(new Variable(VariableType.SENSE_OF_REALITY, emotionVariables.getSenseOfReality().toString()))
+                .add(new Variable(VariableType.PROXIMITY, emotionVariables.getProximity().toString()))
+                .add(new Variable(VariableType.UNEXPECTEDNESS, emotionVariables.getUnexpectedness().toString()))
+                .add(new Variable(VariableType.AROUSAL, emotionVariables.getArousal().toString()));
+
+        Rule rule = RulesBuilder.buildGratitudeRule();
+        Emotion emotion = new Emotion("Gratitude", rule, THRESHOLD);
+
+        if(emotionVariables.getDesirability() != null) {
+            Variable desirability = new Variable(VariableType.DESIRABILITY, emotionVariables.getDesirability().toString());
+            model.add(desirability);
+        }
+
+        if(emotionVariables.getPraiseworthiness() != null) {
+            Variable praiseworthiness = new Variable(VariableType.PRAISEWORTHINESS, emotionVariables.getPraiseworthiness().toString());
+            model.add(praiseworthiness);
+        }
+
+        if(emotionVariables.getExpectationDeviation() != null) {
+            Variable expectationDeviation = new Variable(VariableType.EXPECTATION_DEVIATION, emotionVariables.getExpectationDeviation().toString());
+            model.add(expectationDeviation);
+        }
+
+        try {
+            boolean isGratitude = Evaluator.evaluate(emotion, model);
+            if(isGratitude) {
                 return emotion;
             }
         } catch (Exception ignored) {
--- BichinhoVirtual/app/src/main/java/br/com/ia369/bichinhovirtual/appraisal/EmotionEngineService.java
+++ BichinhoVirtual/app/src/main/java/br/com/ia369/bichinhovirtual/appraisal/EmotionEngineService.java
@@ -21,6 +21,8 @@ import br.com.ia369.bichinhovirtual.room.EmotionRepository;
 public class EmotionEngineService extends Service {
 
     private static final String TAG = EmotionEngineService.class.getSimpleName();
+
+    private static final String INTERVAL_REQUEST = "interval_request";
     public static final int REQUEST_CODE = 42;
     public static final int INTERVAL_TIME = 10 * 1000; // TODO definir intervalo de tempo adequado
 
@@ -33,17 +35,25 @@ public class EmotionEngineService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(TAG, "onStartCommand");
-        scheduleEmotionEngineJob(this);
+
+        if(intent != null) {
+            if(intent.hasExtra(INTERVAL_REQUEST)) {
+                scheduleEmotionEngineJob(this);
+            }
+        }
 
         new GetEmotionVariablesAsyncTask(this).execute();
 
         return super.onStartCommand(intent, flags, startId);
     }
 
+
+    // FIXME alarmManager nao funciona em doze mode
     public static void scheduleEmotionEngineJob(Context context) {
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 
         Intent emotionEngineServiceIntent = new Intent(context, EmotionEngineService.class);
+        emotionEngineServiceIntent.putExtra(INTERVAL_REQUEST, true);
         PendingIntent pendingIntent = PendingIntent.getService(context, REQUEST_CODE, emotionEngineServiceIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         long now = System.currentTimeMillis();
@@ -59,16 +69,19 @@ public class EmotionEngineService extends Service {
     static class GetEmotionVariablesAsyncTask extends AsyncTask<Void, Void, Void> {
         WeakReference<EmotionEngineService> emotionEngineServiceWeakReference;
 
-        public GetEmotionVariablesAsyncTask(EmotionEngineService instance) {
+        GetEmotionVariablesAsyncTask(EmotionEngineService instance) {
             this.emotionEngineServiceWeakReference = new WeakReference<>(instance);
         }
 
         @Override
         protected Void doInBackground(Void... voids) {
+
             Application application = emotionEngineServiceWeakReference.get().getApplication();
             EmotionRepository repository = new EmotionRepository(application);
             EmotionVariables emotionVariables = repository.getEmotionVariable(AppraisalConstants.PERSONALITY_EXTROVERT, AppraisalConstants.INPUT_TEXT_JOY);
+
             if(emotionVariables != null) {
+
                 Emotion fear = Appraisal.evaluateFear(emotionVariables);
                 if(fear != null) {
                     Log.d(TAG, "Fear intensity = "+fear.getIntensity());
@@ -82,7 +95,50 @@ public class EmotionEngineService extends Service {
                 } else {
                     Log.d(TAG, "Not joy");
                 }
+
+                Emotion sadness = Appraisal.evaluateSadness(emotionVariables);
+                if(sadness != null) {
+                    Log.d(TAG, "Sadness intensity = "+sadness.getIntensity());
+                } else {
+                    Log.d(TAG, "Not sadness");
+                }
+
+                Emotion disgust = Appraisal.evaluateReproach(emotionVariables);
+                if(disgust != null) {
+                    Log.d(TAG, "Disgust intensity = "+disgust.getIntensity());
+                } else {
+                    Log.d(TAG, "Not disgust");
+                }
+
+                Emotion anger = Appraisal.evaluateAnger(emotionVariables);
+                if(anger != null) {
+                    Log.d(TAG, "Anger intensity = "+anger.getIntensity());
+                } else {
+                    Log.d(TAG, "Not anger");
+                }
+
+                Emotion satisfaction = Appraisal.evaluateSatisfaction(emotionVariables);
+                if(satisfaction != null) {
+                    Log.d(TAG, "Satisfaction intensity = "+satisfaction.getIntensity());
+                } else {
+                    Log.d(TAG, "Not satisfaction");
+                }
+
+                Emotion distress = Appraisal.evaluateDistress(emotionVariables);
+                if(distress != null) {
+                    Log.d(TAG, "Distress intensity = "+distress.getIntensity());
+                } else {
+                    Log.d(TAG, "Not distress");
+                }
+
+                Emotion gratitude = Appraisal.evaluateGratitude(emotionVariables);
+                if(gratitude != null) {
+                    Log.d(TAG, "Gratitude intensity = "+gratitude.getIntensity());
+                } else {
+                    Log.d(TAG, "Not gratitude");
+                }
             }
+
             return null;
         }
     }
